e58f15cd8c5eb9c8f7c25d677ede91feafd2920f
Gup <gup.dwp@gmail.com>
2016-09-05 15:33:18 +0100

    doi test sept5

--- archive/test.php
+++ archive/test.php
@@ -1,4 +1,4 @@
 
 <?php
-    echo file_get_contents('https://spreadsheets.google.com/feeds/list/19Uc0FgtNDp9EfFhiHZJHG0ax8ZCtdvxSkbZryiOxSKo/oryd3t8/public/basic?alt=json-in-script&callback=callback');
+    echo file_get_contents('https://spreadsheets.google.com/tq?tqx=&tq=SELECT%20A%2C%20B%2C%20C%20&key=19Uc0FgtNDp9EfFhiHZJHG0ax8ZCtdvxSkbZryiOxSKo&gid=1690305366');
     ?>
