ee98aa2e092d7da37b3fc36bb434ba917f58090d
Gup <gup.dwp@gmail.com>
2016-09-05 13:41:33 +0100

    doi test sept5

--- archive/test.php
+++ archive/test.php
@@ -1,9 +1,4 @@
+
 <?php
-
-
-$url = 'http://spreadsheets.google.com/tq?key=19Uc0FgtNDp9EfFhiHZJHG0ax8ZCtdvxSkbZryiOxSKo&gid=1690305366&range=D17:F17';
-
-$content = file_get_contents($url);
-$json = json_decode($content, true);
-echo $content;
-?>
+    echo file_get_contents('https://spreadsheets.google.com/tq?tqx=&tq=SELECT%20A%2C%20B%2C%20C%20&key=19Uc0FgtNDp9EfFhiHZJHG0ax8ZCtdvxSkbZryiOxSKo&gid=1690305366');
+    ?>
