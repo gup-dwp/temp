174a4c16baa9682d913bf7aff6405fce6ce61951
Gup <gup.dwp@gmail.com>
2016-09-05 15:25:31 +0100

    doi test sept5

--- quickstart.php
+++ quickstart.php
@@ -1,27 +1,5 @@
 
 <?php
-require 'vendor/autoload.php';
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-
-define('APPLICATION_NAME', 'Google Sheets API PHP Quickstart');
-define('CREDENTIALS_PATH', '~/.credentials/sheets.googleapis.com-php-quickstart.json');
-define('CLIENT_SECRET_PATH', __DIR__ . '/client_secret.json');
-// If modifying these scopes, delete your previously saved credentials
-// at ~/.credentials/sheets.googleapis.com-php-quickstart.json
-define('SCOPES', implode(' ', array(
-  Google_Service_Sheets::SPREADSHEETS_READONLY)
-));
-
-if (php_sapi_name() != 'cli') {
-  throw new Exception('This application must be run on the command line.');
-}
-
-$serviceRequest = new DefaultServiceRequest("");
-ServiceRequestFactory::setInstance($serviceRequest);
-
-$spreadsheetService = new Google\Spreadsheet\SpreadsheetService();
-$worksheetFeed = $spreadsheetService->getPublicSpreadsheet("spreadsheet-id");
+$html_link  = "http://spreadsheets.google.com/tq?key=19Uc0FgtNDp9EfFhiHZJHG0ax8ZCtdvxSkbZryiOxSKo&gid=1690305366&range=R3:S5";
 
 ?>
